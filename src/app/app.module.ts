f096f26478a91bb5efd27045163fba2b40c083fe
Rodiel Martinez Jimenez <Rodielmj@gmail.com>
2019-01-10 00:37:29 +0100

    added Background component & Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuotesComponent } from "./quotes/quotes.component";
+import { SearchComponent } from "./search/search.component";
+import { BackgroundComponent } from "./background/background.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { QuotesService } from "./services/quotes.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    SearchComponent,
+    BackgroundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [QuotesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
